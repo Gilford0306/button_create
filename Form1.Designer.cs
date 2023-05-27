8e75d581ca9566cb297828e947f2419b8d47c829
Gilford0306 <Gilford0306@gmail.com>
2023-05-27 21:55:30 +0300

    INIT

---
+++ Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace button_create
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Hello WinForms";
+
+            this.BackColor = Color.FromArgb(255, 96, 130, 230);
+            this.Size = new Size(400, 400);
+            this.MaximumSize = new Size(400, 400);
+            this.MinimumSize = new Size(300, 300);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+
+            //
+            //      Button click me
+            //
+            btn = new Button();
+            btn.Text = "Click me";
+            btn.Name = $"btn_0";
+            btn.ForeColor = Color.Green;
+            btn.Size = new Size(90, 100);
+            btn.Location = new Point(0, 0);
+            btn.BackColor = Color.Aquamarine;
+            btn.Font = new Font("Times New Roman", 23, FontStyle.Bold | FontStyle.Italic);
+            btn.Click += Btn_Click;
+
+
+            // add my btn to my form !!!!!
+            this.Controls.Add(btn);
+
+            btn_del = new Button();
+            btn_del.Text = "Del Button";
+            btn_del.Name = $"btn_11";
+            btn_del.ForeColor = Color.Green;
+            btn_del.Size = new Size(90, 100);
+            btn_del.Location = new Point(290, 260);
+            btn_del.BackColor = Color.Aquamarine;
+            btn_del.Font = new Font("Times New Roman", 23, FontStyle.Bold | FontStyle.Italic);
+            btn_del.Click += Btn_DEL;
+            this.Controls.Add(btn_del);
+
+
+
+        }
+
+        Button btn;
+        Button btn_del;
+        #endregion
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace button_create
+{
+    public partial class Form1 : Form
+    {
+        private int posX = 0;
+        private int posY = 0;
+
+        int indx = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Click(object sender, System.EventArgs e)
+        {
+            AddBtn();
+        }
+        private void Btn_DEL(object sender, System.EventArgs e)
+        {
+
+
+            delBtn();
+
+        }
+
+
+        private void AddBtn()
+        {
+                    posX += 100;
+                    if (posX >= this.Width)
+                    {
+                        posX = 100;
+                        posY += 20;
+                    }
+                    Button btn = new Button();
+                    btn.Name = $"btn_{indx.ToString()}";
+                    btn.Text = $"{indx.ToString()}";
+                    Random r = new Random();
+                    btn.BackColor = Color.FromArgb(r.Next(0, 256), r.Next(0, 256), r.Next(0, 256));
+                    btn.Location = new Point(posX, posY);
+                    btn.Click += Btn_Click;
+                    // add my btn to my form !!!!!
+                    this.Controls.Add(btn);
+                    indx++;
+
+        }
+
+        private void delBtn()
+        {
+
+
+            DialogResult res = MessageBox.Show("Do you want to deleted button", "Deleted", MessageBoxButtons.YesNo, MessageBoxIcon.Error);
+            if (res == DialogResult.Yes)
+            {
+                for (int i = indx; i > 1; i--)
+                {
+                    this.Controls.RemoveAt(i);
+                }
+                indx = 1;
+                posX = 0;
+                posY = 0;
+                MessageBox.Show("Button is deleted");
+            }
+            else
+            {
+                MessageBox.Show("Button is not deleted");
+            }
+
+        }
+
+    }
+}
